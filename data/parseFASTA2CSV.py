22fd621964b76bc09ff3b57adc1fd704c2f9b702
mathornton01 <mathornton@smu.edu>
2021-05-05 13:34:39 -0500

    Added aligned dataset

---
+++ data/parseFASTA2CSV.py
@@ -0,0 +1,27 @@
+filename = "gisaid_hcov-19_2020_05_18_18.fasta"
+ofilename = "unaligned.csv"
+headers = []
+sequences = [] 
+newseq = False
+seq = ""
+with open(filename,'r') as fastain: 
+    for line in fastain: 
+        if line[0] == ">": 
+            headers.append(line.strip())
+            if (seq!=""):
+                sequences.append(seq)
+            newseq = True
+        else:
+            if newseq:
+                newseq = False
+                seq = ""
+            else: 
+                seq = seq+line.strip()
+
+sequences.append(seq)
+
+with open(ofilename,'w') as csvout: 
+    csvout.write("Header,Sequence\n");
+    for i in range(len(headers)):
+        csvline = headers[i] + "," + sequences[i] + "\n";
+        csvout.write(csvline)
